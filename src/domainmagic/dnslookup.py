6c830094cc8a1e64d699a39518cc72941bcf8095
gryphius <oli@fuglu.org>
2014-06-19 11:01:58 +0200

    move methods into class

--- src/domainmagic/dnslookup.py
+++ src/domainmagic/dnslookup.py
@@ -34,26 +34,30 @@ def _make_results(question,qtype,resolveranswer):
      
     return results
 
-def lookup(question,qtype='A'):
-    """returns a list of DNSLookupResult"""
-    r=resolver.Resolver()
-    resolveranswer = r.query(question, qtype)
-    results=_make_results(question,qtype,resolveranswer)
-    
-    return results
 
-def _lookup_in_thread(question,callback, callbackarg,qtype='A'):
-    ret=lookup(question,qtype=qtype)
-    callback(ret,callbackarg)
+class DNSLookup(object):
+    def __init__(self):
+        self.resolver=resolver.Resolver()
     
-
-def lookup_async(question,callback, callbackarg, qtype='A'):
-     #TODO: for now we use a simple thread -> convert to threadpool later
-     thread.start_new(_lookup_in_thread, (question,callback, callbackarg, qtype),)
+    def lookup_sync(self,question,qtype='A'):
+        """returns a list of DNSLookupResult"""
+        resolveranswer = self.resolver.query(question, qtype)
+        results=_make_results(question,qtype,resolveranswer)
+        return results
+    
+    def _lookup_in_thread(self,question,callback, callbackarg,qtype='A'):
+        ret=lookup(question,qtype=qtype)
+        callback(ret,callbackarg)
+        
+    
+    def lookup_async(self,question,callback, callbackarg, qtype='A'):
+         #TODO: for now we use a simple thread -> convert to threadpool later
+         thread.start_new(self._lookup_in_thread, (question,callback, callbackarg, qtype),)
      
      
 if __name__=='__main__':
-    print lookup('wgwh.ch')
+    d=DNSLookup()
+    print d.lookup_sync('wgwh.ch')
     
     
     
