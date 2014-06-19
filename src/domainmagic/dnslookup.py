a87de0169a3682213452ba6ed2618dd319a1c5b9
gryphius <oli@fuglu.org>
2014-06-19 11:47:39 +0200

    parallel lookup

--- src/domainmagic/dnslookup.py
+++ src/domainmagic/dnslookup.py
@@ -1,5 +1,8 @@
 import thread
 from dns import resolver
+from tasker import ThreadPool,Task,TaskGroup
+import time
+import logging
 
 class DNSLookupResult(object):
     def __init__(self):
@@ -38,26 +41,60 @@ def _make_results(question,qtype,resolveranswer):
 class DNSLookup(object):
     def __init__(self):
         self.resolver=resolver.Resolver()
+        self.threadpool=ThreadPool(10)
+        
     
-    def lookup_sync(self,question,qtype='A'):
-        """returns a list of DNSLookupResult"""
+    def lookup(self,question,qtype='A'):
+        """lookup one record returns a list of DNSLookupResult"""
         resolveranswer = self.resolver.query(question, qtype)
         results=_make_results(question,qtype,resolveranswer)
         return results
     
-    def _lookup_in_thread(self,question,callback, callbackarg,qtype='A'):
-        ret=lookup(question,qtype=qtype)
-        callback(ret,callbackarg)
+    def lookup_multi(self,questions, qtype='A'):
+        """lookup a list of multiple records of the same qtype. the lookups will be done in parallel"""
         
-    
-    def lookup_async(self,question,callback, callbackarg, qtype='A'):
-         #TODO: for now we use a simple thread -> convert to threadpool later
-         thread.start_new(self._lookup_in_thread, (question,callback, callbackarg, qtype),)
+        global result
+        result=None
+        def _waitforresult(taskgroup):
+            global result
+            tempresult={}
+            for task in taskgroup.tasks:
+                assert task.done
+                tempresult[task.args[0]]=task.result
+            result=tempresult
+            
+        
+        tg=TaskGroup(completecallback=_waitforresult)
+        for question in questions:
+            tg.add_task(self.lookup,args=(question,qtype))
+        
+        self.threadpool.add_task(tg)
+        
+        while result==None:
+            pass
+            
+        return result
+        
+        
+        
+         
+         
      
      
 if __name__=='__main__':
+    logging.basicConfig(level=logging.DEBUG)
     d=DNSLookup()
-    print d.lookup_sync('wgwh.ch')
+    #print "Sync lookup:"
+    #print d.lookup_sync('wgwh.ch')
+    
+    
+    print "lookup start"
+    start=time.time()
+    result=d.lookup_multi(['wgwh.ch','heise.de','slashdot.org'])
+    end=time.time()
+    diff=end-start
+    print "lookup end, time=%.4f"%diff
+    print result
     
     
     
