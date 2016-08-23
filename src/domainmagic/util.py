e846d875b0f8bacddf85ec6a771a25131ade3d5b
neonknight <github@neonknight.ch>
2016-08-23 13:28:12 +0200

    autopep8

--- src/domainmagic/util.py
+++ src/domainmagic/util.py
@@ -2,30 +2,30 @@
 import collections
 
 
-
-def dict_path(l,node,path=None):
+def dict_path(l, node, path=None):
     """walk list l through dict l and return a list of all nodes found up until a leaf node"""
-    if path==None:
-        path=[]
+    if path is None:
+        path = []
 
-    if len(l)==0: #list is finished
+    if len(l) == 0:  # list is finished
         return path
 
-    if not isinstance(node,collections.Mapping): #leafnode
-        if l[0]==node:
+    if not isinstance(node, collections.Mapping):  # leafnode
+        if l[0] == node:
             path.append(node)
         return path
 
     if l[0] in node:
         path.append(l[0])
-        return dict_path(l[1:],node[l[0]],path)
+        return dict_path(l[1:], node[l[0]], path)
 
     return path
 
+
 def dict_update(d, u):
     """add dict u into d changing leafnodes to dicts where necessary"""
-    if not isinstance(d,collections.Mapping):
-        d={d:{}}
+    if not isinstance(d, collections.Mapping):
+        d = {d: {}}
     for k, v in u.iteritems():
         if isinstance(v, collections.Mapping):
             r = dict_update(d.get(k, {}), v)
@@ -34,36 +34,35 @@ def dict_update(d, u):
             d[k] = u[k]
     return d
 
-def dict_topdown_iterator(d,path=None,skiptop=False):
+
+def dict_topdown_iterator(d, path=None, skiptop=False):
     """walk through a dict and list all possible paths. Optional path prepends additional path elements
     if skiptop is True, skips the current level and drills down to the next level
     """
 
-    allpaths=[]
-    if path==None:
-        path=[]
-    currentlevel=sorted(d.keys())
+    allpaths = []
+    if path is None:
+        path = []
+    currentlevel = sorted(d.keys())
     if not skiptop:
         for node in currentlevel:
-            allpaths.append(path[:]+[node,])
+            allpaths.append(path[:] + [node, ])
 
-    #drill down
+    # drill down
     for node in currentlevel:
         if isinstance(d[node], collections.Mapping):
-            for result in dict_topdown_iterator(d[node],path=path+[node,],skiptop=False):
+            for result in dict_topdown_iterator(d[node], path=path + [node, ], skiptop=False):
                 allpaths.append(result)
 
-    for path in sorted(allpaths,key=len):
+    for path in sorted(allpaths, key=len):
         yield path
-
 
 
 def list_to_dict(l):
     """translate a list into a tree path"""
-    d={}
-    if len(l)==0:
+    d = {}
+    if len(l) == 0:
         return d
     else:
-        d[l[0]]=list_to_dict(l[1:])
+        d[l[0]] = list_to_dict(l[1:])
         return d
-
