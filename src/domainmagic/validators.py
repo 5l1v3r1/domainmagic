6ab492029581f892df38f0aa17ce26d65f703772
neonknight <github@neonknight.ch>
2018-01-17 16:24:57 +0100

    validator: add email address validator

--- src/domainmagic/validators.py
+++ src/domainmagic/validators.py
@@ -15,6 +15,7 @@ REGEX_CIDRV6 = REGEX_IPV6 + """\/(?:12[0-8]|1[01][0-9]|[1-9]?[0-9])"""
 
 REGEX_HOSTNAME = """^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$"""
 
+REGEX_EMAIL_LHS = """^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*$"""
 
 
 def _apply_rgx(rgx, content):
@@ -67,3 +68,20 @@ def is_hostname(content, check_valid_tld=False, check_resolvable=False):
             return False
 
     return True
+
+
+def is_email(content, check_valid_tld=False, check_resolvable=False):
+    if not '@' in content:
+        return False
+    
+    lhs, domain = content.split('@', 1)
+    
+    if not is_hostname(domain, check_valid_tld, check_resolvable):
+        return False
+    
+    if not _apply_rgx(REGEX_EMAIL_LHS, lhs):
+        return False
+    
+    return True
+
+
